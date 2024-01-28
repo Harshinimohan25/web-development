88b3b82d65e83fe871708b215d8b68a484a54bd4
Harshinimohan25 <146054944+Harshinimohan25@users.noreply.github.com>
2024-01-28 19:02:33 +0530

    Add files via upload

---
+++ task1/portfolio.js
@@ -0,0 +1,10 @@
+
+var options = {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+};
+
+var typed = new Typed("#typed-text", options);
